df8846ea7dd852453681c3866746aee529a0c5bb
santyskill <santyago67sm@gmail.com>
2023-01-21 09:47:16 +0300

    edit game

--- game.py
+++ game.py
@@ -1,17 +1,4 @@
 import numpy as np
-number=np.random.randint(1, 101)# загадываем число
-count = 0
-
-while True:
-    count+=1
-    predict_number = int(input("Угадай число от 1 до 100 :"))
-    
-    if predict_number > number:
-        print('Число должно быть меньше!')
-        
-    elif predict_number < number:
-        print('Число должно быть больше!')
-        
-    else:
-        print(f'Вы угадали число! Это число = {number}, за {count} попыток')
-        break
+import numpy as np
+simplelist = [19, 242, 14, 152, 142, 1000]
+print((sum(simplelist))/len(simplelist))
